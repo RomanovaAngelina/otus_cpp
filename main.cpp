2b9b6fc9e5fc594ecd5256a4863cbc7c6c3f0206
romanova <romanova@protei.ru>
2020-11-17 19:24:04 +0300

    add sketch homework

--- main.cpp
+++ main.cpp
@@ -1,10 +1,28 @@
-#include "lib.h"
+#include <memory>
 
-#include <iostream>
+#include "main_window.h"
 
-int main (int, char **)
-{
-    std::cout << "Version: " << version() << std::endl;
+using namespace tintenschaft;
 
-    return 0;
+int main() {
+    MainWindow mainWindow { };
+    mainWindow.show();
+
+    auto &canvas { mainWindow.getCanvas() };
+
+    auto &doc { canvas.getSketch() };
+    doc.newFile("Mono Lisa.svg");
+
+    canvas.setBackgroundColor(Color(colors::WHITE));
+    auto face { Ellipse::create({20, 0}, {80, 100}) };
+    auto leftEye { Ellipse::create({30, 30}, {40, 40}) };
+    auto rightEye { Ellipse::create({60, 30}, {70, 40}) };
+
+    canvas.addShape(face);
+    canvas.addShape(leftEye);
+    auto rightEyeUid { canvas.addShape(rightEye) };
+
+    canvas.deleteShape(rightEyeUid);
+
+    doc.saveFile();
 }
